14af60d5f3ad8a98ec0c453b33fed9a2f7c17895
ZoniBoy00 <34036787+ZoniBoy00@users.noreply.github.com>
2024-01-26 22:33:51 +0200

    Delete server directory

--- server/sv_rob.lua
+++
@@ -1,8 +0,0 @@
-Citizen.CreateThread(function()
-    while true do
-        if GetCurrentResourceName() ~= 'ox_playerRob' then
-            print('Change resource ' .. GetCurrentResourceName() .. ' name to ox_playerRob')
-        end
-        Citizen.Wait(60000)
-    end
-end)
