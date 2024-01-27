9542faeb585ae690d07183d7570d90dddef3fa2a
ZoniBoy00 <34036787+ZoniBoy00@users.noreply.github.com>
2024-01-27 18:34:19 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,15 @@
 fx_version 'cerulean'
+lua54 'yes'
 games {'gta5'}
 
 --INFO --
 name        'P_playerRob'
 author      'Projekti'
-version     '1.0.1'
+version     '1.0.2'
+
+shared_script {
+    '@ox_lib/init.lua'
+}
 
 client_scripts {
     'client/*.lua'
@@ -12,4 +17,9 @@ client_scripts {
 
 server_scripts {
     'server/*.lua'
+}
+
+dependencies {
+	'ox_inventory',
+    'ox_lib'
 }
